b7d92fcb46e672a2ceb572ee1f2966a2e3d05d59
bastismoj19 <151817191+bastismoj19@users.noreply.github.com>
2024-05-08 12:22:07 +0800

    modified

--- Portfolio/index.php
+++ Portfolio/index.php
@@ -5,28 +5,38 @@
     <link rel="stylesheet" href="assets/css/bulma.css">
     <script src="assets/js/jquery.js"></script>
     <style>
+        body {
+            color: white;
+            background: url('../assets/image/bgimg.jpg') no-repeat;
+            background-size: cover;
+            background-position: center;
+        }
         #form {
-            background-color: cyan;
+            background-color: pink;
             width: 25%;
             margin: 120px auto;
             padding: 30px;
+            margin-top: -40%;
             box-shadow: 10px 10px 10px rgb(82, 11, 77);
             border-radius: 10px;
             color: black;
-            }
+            z-index: 100;
+        }
     </style>
 
 </head>
 <body>
+    <img src="assets/image/bgimg.jpg" alt="">
     <div id="form" method="post">
         <form action="">
             <label>Username:</label><br>
-            <input type="text" id="username" class="input" name="username" required><br>
+            <input style="background: transparent; border-color: pink;" type="text" id="username" class="input" name="username" required><br>
             <label>Password:</label><br>
-            <input type="password" id="password" class="input" name="password" required><br><br>
-            <button class="button loginButton">Login</button>
+            <input style="background: transparent; border-color: pink;" type="password" id="password" class="input" name="password" required><br><br>
+            <button class="button loginButton" style="margin-left: 37%; background: transparent; border-color: pink; ">Login</button>
         </form>
     </div>
+    
     
     <script>
         $(function(){
